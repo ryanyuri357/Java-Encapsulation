f23cc763ab17a21599a30aba2ecd36242b4b14d4
ryanyuri357 <ryan.yuri357@gmail.com>
2021-07-07 21:03:03 -0500

    Initial Commit

---
+++ src/Java/Encapsulation/Main.java
@@ -0,0 +1,20 @@
+package Java.Encapsulation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + ". New total print count for printer = "
+                + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + ". New total print count for printer = "
+                + printer.getPagesPrinted());
+
+
+
+
+    }
+}
---
+++ src/Java/Encapsulation/Printer.java
@@ -0,0 +1,41 @@
+package Java.Encapsulation;
+
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = (tonerLevel > -1 && tonerLevel <= 100) ? tonerLevel : -1;
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if(tonerAmount > 0 && tonerAmount <= 100) {
+            if(this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            } else {
+                this.tonerLevel += tonerAmount;
+                return this.tonerLevel;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if(this.duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in Duplex Mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
